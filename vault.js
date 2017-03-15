48a47c1c4f51a21bf2f8fb881d3a03a08cccbd8b
Charles1104 <charles.xavier.verleyen@gmail.com>
2017-03-14 19:53:16 -1000

    All tests passed

--- vault.js
+++ vault.js
@@ -1,4 +1,25 @@
 'use strict';
 module.exports = function() {
-  
+  var newObject = {};
+
+  function getValue(key){
+    if (newObject[key] === undefined){
+      return null;
+    }
+    else{
+      return newObject[key];
+    }
+  }
+
+  function setValue(key,value){
+
+    newObject[key]=value;
+  }
+
+  var foo = {
+    getValue,
+    setValue
+  };
+
+  return foo;
 };
